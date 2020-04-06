e0c284914aee11ae189f0a7da1115ac1eeba0aab
Andre Wolden <wolden.andre@gmail.com>
2020-04-06 21:12:56 +0200

    starter på nytt design på kviteringssiden

--- src/app/components/pages/confirmation-page/ConfirmationPage.tsx
+++ src/app/components/pages/confirmation-page/ConfirmationPage.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
-import { FormattedHTMLMessage, FormattedMessage, useIntl } from 'react-intl';
-import { Ingress, Innholdstittel } from 'nav-frontend-typografi';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { Ingress, Innholdstittel, Undertittel } from 'nav-frontend-typografi';
 import Box from 'common/components/box/Box';
 import CheckmarkIcon from 'common/components/checkmark-icon/CheckmarkIcon';
 import Page from 'common/components/page/Page';
 import bemUtils from 'common/utils/bemUtils';
 import intlHelper from 'common/utils/intlUtils';
-import getLenker from 'app/lenker';
 import { KvitteringInfo } from '../../../søknad/SøknadRoutes';
 import './confirmationPage.less';
+import { Panel } from 'nav-frontend-paneler';
+import AlertStripe from 'nav-frontend-alertstriper';
+import { Knapp } from 'nav-frontend-knapper';
 
 const bem = bemUtils('confirmationPage');
 
@@ -33,22 +35,45 @@ const ConfirmationPage: React.FunctionComponent<Props> = ({ kvitteringInfo }) =>
                 <Ingress>
                     <FormattedMessage id="page.confirmation.undertittel" />
                 </Ingress>
+
+                <Panel border={true} className={'luftOver'}>
+                    <AlertStripe type="advarsel" form="inline">
+                        Obs! Denne informasjonen forsvinner når du lukker den. Det er derfor viktig at du leser gjennom
+                        før du går videre.
+                        <div>Siden kan printes ut, slik at du kan gi utskrift til arbeidsgiveren din.</div>
+                    </AlertStripe>
+                </Panel>
                 <ul className="checklist">
                     <li>
-                        <FormattedHTMLMessage id="page.confirmation.hvaSkjer1" />
+                        <b>Dette er en bekreftelse på at vi har mottatt søknaden din. </b> På grunn av stor pågang er
+                        det vanskelig å si når søknaden vil bli synlig for deg på Ditt NAV.
+                        <p>Når søknaden din er ferdigbehandlet, får du et svar fra oss.</p>
                     </li>
                     <li>
-                        <FormattedHTMLMessage id="page.confirmation.hvaSkjer2" />
+                        Du må be arbeidsgiver om å sende inntektsmelding til oss. Det er viktig at arbeidsgiver krysser
+                        av for at inntektsmeldingen gjelder omsorgspenger.
+                        <p>Vi kontakter deg hvis vi trenger flere opplysninger i saken din.</p>
                     </li>
                     <li>
-                        <FormattedHTMLMessage
-                            id="page.confirmation.hvaSkjer3"
-                            values={{
-                                lenke: getLenker(intl.locale).saksbehandlingstider
-                            }}
-                        />
+                        Du kan skrive ut denne informasjonssiden og gi utskriften til arbeidsgiveren din. Hvis du er
+                        registrert med flere arbeidsgivere, får du en utskrift til hver av dem.
                     </li>
                 </ul>
+                <div className={'skrivUtKnapp'}>
+                    <Knapp type={'hoved'}>Skriv ut denne siden nå</Knapp>
+                </div>
+                <div className={'kviteringsBlokk'}>
+                    Hvis du ikke kan skrive ut denne informasjonssiden, kan du ta bilde av den. Husk også å ta bilde av
+                    informasjonen som kommer under, som du kan gi til arbeidsgiver.
+                </div>
+
+                <Panel border={true} className={'luftOver'}>
+                    <Undertittel>
+                        Til Arbeidsgiver 2AS
+                        {/*TODO: Fullfør ihht design og insert info om arbeidsgiver*/}
+                        <p>kommer...</p>
+                    </Undertittel>
+                </Panel>
             </Box>
         </Page>
     );
