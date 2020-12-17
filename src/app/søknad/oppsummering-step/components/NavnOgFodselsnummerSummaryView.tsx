8cc8a5358f939d48d436237b7ca885978fe7aff3
Aleksei Poroshin <aleksei.poroshin@nav.no>
2020-12-17 14:51:58 +0100

    Fikset fnr i oppsummeringen

--- src/app/søknad/oppsummering-step/components/NavnOgFodselsnummerSummaryView.tsx
+++ src/app/søknad/oppsummering-step/components/NavnOgFodselsnummerSummaryView.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Normaltekst } from 'nav-frontend-typografi';
 import Box from 'common/components/box/Box';
 import { formatName } from 'common/utils/personUtils';
+import intlHelper from '@navikt/sif-common-core/lib/utils/intlUtils';
+import { useIntl } from 'react-intl';
 
 export interface Props {
     fornavn?: string;
@@ -12,10 +14,11 @@ export interface Props {
 
 export const NavnOgFodselsnummerSummaryView: React.FC<Props> = (props: Props): JSX.Element => {
     const { fornavn, etternavn, mellomnavn, fødselsnummer } = props;
+    const intl = useIntl();
     return (
         <Box margin={'l'}>
             {fornavn && etternavn && <Normaltekst>{formatName(fornavn, etternavn, mellomnavn)}</Normaltekst>}
-            <Normaltekst>Fødselsnummer: {fødselsnummer}</Normaltekst>
+            <Normaltekst>{intlHelper(intl, `steg.oppsummering.søker.fnr`, { fødselsnummer })}</Normaltekst>
         </Box>
     );
 };
