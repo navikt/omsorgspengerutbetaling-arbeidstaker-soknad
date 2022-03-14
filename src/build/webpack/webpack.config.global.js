0d09b42a0182d3ae8f56d0600fc15b2c9f2f49e3
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-03-14 18:52:07 +0100

    Fiks sanity Api, pkg upd

--- src/build/webpack/webpack.config.global.js
+++ src/build/webpack/webpack.config.global.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
@@ -38,11 +37,6 @@ const webpackConfig = {
                 test: /\.less$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
             },
-            {
-                test: /\.svg$/,
-                loader: 'svg-sprite-loader',
-                options: {},
-            },
         ],
     },
     plugins: [
@@ -53,9 +47,6 @@ const webpackConfig = {
         new MiniCssExtractPlugin({
             filename: 'css/[name].css?[fullhash]-[chunkhash]-[name]',
             linkType: 'text/css',
-        }),
-        new SpriteLoaderPlugin({
-            plainSprite: true,
         }),
     ],
 };
