e30efb03e9f7cf82d3995ee13b249c502d0f671e
Frode Hansen <frode.hansen@bekk.no>
2022-12-07 15:56:00 +0100

    Jeg bekrefter tekst

--- src/app/pages/velkommen-page/velkommenPageMessages.ts
+++ src/app/pages/velkommen-page/velkommenPageMessages.ts
@@ -16,10 +16,10 @@ export const velkommenPageMessages: MessageFileFormat = {
         'page.velkommen.omSøknaden.3':
             'Du må svare på alle spørsmålene for å kunne gå videre. Hvis du mangler etterspurt dokumentasjon, kan du ettersende det så snart du kan.',
         'page.velkommen.omSøknaden.4': 'Om hvordan vi innhenter opplysninger om deg',
-        'page.velkommen.form.bekreftLabel': 'Jeg bekrefter at jeg har lest og forstått',
+        'page.velkommen.form.bekreftLabel': 'Jeg bekrefter at jeg har forstått mitt ansvar som søker',
         'page.velkommen.form.ansvar.tittel': 'Ditt ansvar som søker',
         'page.velkommen.form.ansvar.list.1':
-            'Jeg forstår at hvis jeg gir uriktige opplysninger, kan det få konsekvenser for retten min til det jeg søker om',
+            'Jeg forstår at hvis jeg gir uriktige opplysninger, kan det få konsekvenser for retten min til det jeg søker om.',
         'page.velkommen.form.ansvar.list.2.1': 'Jeg har lest og forstått det som står på',
         'page.velkommen.form.ansvar.list.2.2': 'nav.no/rett og plikt',
         'page.velkommen.form.ansvar.list.2.3': '.',
