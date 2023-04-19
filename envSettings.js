785b5710fa4f5c9c6807f8030c35f901937c56bc
Aleksei Poroshin <aleksei.poroshin@nav.no>
2023-04-19 08:48:41 +0200

    intern ingress

--- envSettings.js
+++ envSettings.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const envSettings = () => {
     const API_URL = process.env.API_URL;
     const FRONTEND_API_PATH = process.env.FRONTEND_API_PATH;
+    const VEDLEGG_API_URL = process.env.VEDLEGG_API_URL;
     const FRONTEND_VEDLEGG_URL = process.env.FRONTEND_VEDLEGG_URL;
     const PUBLIC_PATH = process.env.PUBLIC_PATH;
     const LOGIN_URL = process.env.LOGIN_URL;
@@ -18,6 +19,7 @@ const envSettings = () => {
     const appSettings = `
      window.appSettings = {
          API_URL: '${API_URL}',
+         VEDLEGG_API_URL: '${VEDLEGG_API_URL}',
          FRONTEND_API_PATH: '${FRONTEND_API_PATH}',
          FRONTEND_VEDLEGG_URL:'${FRONTEND_VEDLEGG_URL}',
          PUBLIC_PATH: '${PUBLIC_PATH}',
